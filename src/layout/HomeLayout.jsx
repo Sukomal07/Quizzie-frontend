3cb7d46dd176b92fad83f1caeea0055459fb2ef1
Sukomal07 <hypersukomal@gmail.com>
2024-01-20 20:23:47 +0530

    added active tab

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,12 +1,23 @@
 import '../styles/HomeLayout.css'
 
+import { useState } from 'react';
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import Analytics from '../pages/dashboard/Analytics'
+import CreateQuiz from '../pages/dashboard/CreateQuiz'
+import Trending from '../pages/dashboard/Trending'
 import { logout } from '../redux/slices/AuthSlice';
 
 function HomeLayout({ children }) {
+    const [activeTab, setActiveTab] = useState('dashboard');
+
     const dispatch = useDispatch();
+
+    function handleTabClick(tab) {
+        setActiveTab(tab);
+    }
+
     async function onLogout() {
         await dispatch(logout())
     }
@@ -16,18 +27,36 @@ function HomeLayout({ children }) {
                 <div className="sidebar">
                     <Link to={'/dashboard'} className='title'>QUIZZIE</Link>
                     <div className='sidebar-menu'>
-                        <span>Dashboard</span>
-                        <span>Analytics</span>
-                        <span>Create Quiz</span>
+                        <span
+                            className={activeTab === 'dashboard' ? 'tabActive' : ''}
+                            onClick={() => handleTabClick('dashboard')}
+                        >Dashboard</span>
+                        <span
+                            className={activeTab === 'analytics' ? 'tabActive' : ''}
+                            onClick={() => handleTabClick('analytics')}
+                        >Analytics</span>
+                        <span
+                            className={activeTab === 'createQuiz' ? 'tabActive' : ''}
+                            onClick={() => handleTabClick('createQuiz')}
+                        >Create Quiz</span>
                     </div>
                     <div className='logout'>
                         <hr />
                         <span onClick={onLogout}>Logout</span>
                     </div>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    {activeTab === 'dashboard' ? (
+                        <Trending />
+                    ) : activeTab === 'analytics' ? (
+                        <Analytics />
+                    ) : activeTab === 'createQuiz' ? (
+                        <CreateQuiz />
+                    ) : (
+                        children
+                    )}
+                </div>
             </div>
-            {children}
         </div>
     )
 }
