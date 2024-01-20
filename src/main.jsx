faa7adc263f201049f579542907ed4a6dbb4ec96
Sukomal07 <hypersukomal@gmail.com>
2024-01-20 20:22:09 +0530

    remove react strict mood

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import { Provider } from 'react-redux'
@@ -8,12 +7,10 @@ import App from './App.jsx'
 import store from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <Toaster />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
 )
